c9d0a60df0c7f01099eecb006d61b0c204300363
Farrukh <farrukhb30@gmail.com>
2021-03-30 12:36:07 +0200

    The project named as "ProjectGroup07"!

---
+++ src/main/java/ProjectGroup07.java
@@ -0,0 +1,15 @@
+
+
+public class ProjectGroup07 {
+
+    public static void main(String[] args) {
+
+
+        ProjectGroup07.mainMethod("Tahar");
+
+    }
+
+    static void mainMethod(String name){
+        System.out.println("Hello "+ name + "!\nThis is our DockerImage practice assignment.");
+    }
+}
---
+++ src/test/java/ProjectGroup07Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectGroup07Test {
+
+
+    @Test
+    void testMainMethod(){
+
+    }
+}
