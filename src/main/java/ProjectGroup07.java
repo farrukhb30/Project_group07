67a34bf95b8de51efa0cf502a05be25b06193038
Farrukh <farrukhb30@gmail.com>
2021-03-30 20:36:53 +0200

    Docker commit

--- src/main/java/ProjectGroup07.java
+++ src/main/java/ProjectGroup07.java
@@ -4,9 +4,7 @@ public class ProjectGroup07 {
 
     public static void main(String[] args) {
 
-
-//        ProjectGroup07.mainMethod("Tahar");
-
+       ProjectGroup07.mainMethod("Tahar");
     }
 
     static String mainMethod(String name){
