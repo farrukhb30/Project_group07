57f75839dec3909e76be947e355cccdabd8004ac
Farrukh <farrukhb30@gmail.com>
2021-03-31 15:01:50 +0200

    Pom.xml

--- src/main/java/ProjectGroup07.java
+++ src/main/java/ProjectGroup07.java
@@ -4,7 +4,7 @@ public class ProjectGroup07 {
 
     public static void main(String[] args) {
 
-       ProjectGroup07.mainMethod(" Tahar");
+       ProjectGroup07.mainMethod("Tjenare Tahar");
         System.out.println("This is the Docker testing!!!!");
     }
 
