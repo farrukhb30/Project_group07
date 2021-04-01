db02e8807cad8329dc745e96bc8ae06d619a3e6a
Farrukh <farrukhb30@gmail.com>
2021-04-01 11:16:46 +0200

    changed Dockerfile+pom.xml+mainclass

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM adoptopenjdk:11-jre
 WORKDIR /
+COPY ~/.m2/repository/com/google/guava/14.0.1/guava.jar /
 ADD ./target/ProjectGroup07-1.0-SNAPSHOT.jar ProjectGroup07-1.0-SNAPSHOT.jar
 EXPOSE 8081
 CMD java -jar /ProjectGroup07-1.0-SNAPSHOT.jar
+CMD ["sh run.sh", "/guava.jar"]
+
--- src/main/java/ProjectGroup07.java
+++ src/main/java/ProjectGroup07.java
@@ -1,10 +1,11 @@
+import static com.google.common.base.CharMatcher.whitespace;
 
 
 public class ProjectGroup07 {
 
     public static void main(String[] args) {
 
-        System.out.println(ProjectGroup07.greetings(" ProjectGroup07"));
+        System.out.println(ProjectGroup07.greetings(whitespace().trimFrom("            ProjectGroup07")));
     }
 
     static String greetings(String name){
