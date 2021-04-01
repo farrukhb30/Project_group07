860179fe113a85957aa86970ac9f891533f40b2e
Farrukh <farrukhb30@gmail.com>
2021-04-01 12:30:52 +0200

    changed Dockerfile with all 3 dependencies!

--- src/main/java/ProjectGroup07.java
+++ src/main/java/ProjectGroup07.java
@@ -1,11 +1,14 @@
+import org.apache.commons.text.translate.UnicodeEscaper;
 import static com.google.common.base.CharMatcher.whitespace;
 
 
 public class ProjectGroup07 {
+    static UnicodeEscaper ue = UnicodeEscaper.above(0);
 
     public static void main(String[] args) {
 
         System.out.println(ProjectGroup07.greetings(whitespace().trimFrom("            ProjectGroup07")));
+        System.out.println(ue.translate(ProjectGroup07.greetings("ProjectGroup07")));
     }
 
     static String greetings(String name){
